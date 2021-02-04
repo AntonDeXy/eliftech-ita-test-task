1d01827a9e0e7566a849fec8480c49fb3834f849
AntonDeXy <podolyananton@gmail.com>
2021-02-04 19:38:50 +0200

    port from .env

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const PORT = 5000 | process.env.PORT
+const PORT = process.env.PORT || 5000 
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
